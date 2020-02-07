dc84facb3fa36935b58c4dc91a9d4a4908e8f5d3
Dipak Pradip Pachpande <Dipak.Pachpande@india.eclerx.com>
2020-02-07 21:44:41 +0530

    After completion of Video 157

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
   return (
     <div className="App">
-      <h1>Burger Buider App...!</h1>
+      <h1>-Burger Buider App-</h1>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
   );
 }
